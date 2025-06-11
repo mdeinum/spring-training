116f62335a0c2626855aaa45cf7731c1d8265a45
Marten Deinum <marten@deinum.biz>
2025-06-11 09:25:52 +0200

    Add validation to book

--- 00-library-shared/src/main/java/biz/deinum/library/Book.java
+++ 00-library-shared/src/main/java/biz/deinum/library/Book.java
@@ -1,10 +1,13 @@
 package biz.deinum.library;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
-import java.util.Set;
 import org.springframework.data.annotation.Id;
 
-public record Book(@Id Long id, String isbn, String title, String... authors) {
+public record Book(@Id Long id, @NotBlank @Size(min=9, max=13) String isbn,
+                   @NotBlank String title, @NotEmpty String... authors) {
 
   public Book(String isbn, String title, String... authors) {
     this(null, isbn, title, authors);
--- 00-library-shared/src/main/java/biz/deinum/library/BookRepository.java
+++ 00-library-shared/src/main/java/biz/deinum/library/BookRepository.java
@@ -2,10 +2,12 @@ package biz.deinum.library;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jdbc.repository.query.Query;
 
 public interface BookRepository {
 
   List<Book> findAll();
+  @Query
   Optional<Book> findByIsbn(String isbn);
   Book save(Book book);
 }
